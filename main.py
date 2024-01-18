ccb420ea6044d4659a56b881dd7305eebb443da7
Skyaner <138205732+Skyaner@users.noreply.github.com>
2024-01-18 17:48:37 +0100

    update

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 
 
+print("This is a simple number guessing game.\n")
+print("Range \n")
+
 i = 1
 
 while i == 1:
@@ -8,48 +11,62 @@ while i == 1:
     try:
         min = int(input("Minimum: "))
     except:
-        print("This is not a valid input!!!")
+        print("This is not a valid input!!! \n")
         continue
                 
     try:
         max = int(input("Maximum: "))     
     except:
-        print("This is not a valid input!!!")
+        print("This is not a valid input!!! \n")      
         continue
     
     # range
-    if max > min:
+    if max > min and min >= 0:
         Number  = random.randint(min,max)
         break
+    elif min < 0:
+        print("Your minimum should start at 0 or higher \n")
+       
     else:
-        print("Your minimum is greater than or equal to your maximum")
+        print("Your minimum is greater than or equal to your maximum \n")       
         continue
     
 print(Number)
 
 x = -1
+c = 0
 
 while x  != Number:
     # range
-    print(f"The number is between [{min},{max}]")
+    print(f"The number is between [{min},{max}] \n")
     
     # input check
     try:
         x = int(input("Your guess: "))
     except:
-        print("Please enter an integer")
+        print("Please enter an integer \n")
         continue 
+    
+    # counter
+    c += 1
     
     # game logic
     if x > max or x < min:
-        print("Your guess is outside of your range") 
+        print("Your guess is outside of your range \n")
+     
     elif x - 5 > Number :
-        print("You guessed to high")
+        print("You guessed to high \n")
+        
     elif x > Number:
-        print("You guessed a little to high")
+        print("You guessed a little to high \n")
+       
     elif x + 5 < Number:
-        print("You guessed to low")
+        print("You guessed to low \n")
+         
     elif x < Number:
-        print("You guessed a little to low")
+        print("You guessed a little to low \n")
+        
     else:
-        print("Congratulation, you did it")
+        print("Congratulation, you did it \n")
+        print(f"It took you {c} tries")
+         
